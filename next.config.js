48d2ecb6440f91aaa3850e43612748f93bac5fed
Sherwin Jay Suva <sherwinjaysuva@Sherwins-Mac-mini.local>
2022-03-17 02:36:48 +0800

    update fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
 module.exports = {
-/* config options here */
-  env: {
-    MOVIE_DATABASE_ID: process.env.MOVIE_DATABASE_ID,
-  },
-  images:{
-    domains: ['image.tmdb.org'],
+  /* config options here */
+      env: {
+          MOVIE_DATABASE_ID: process.env.MOVIE_DATABASE_ID,
+      },
+      images:{
+        domains: ['image.tmdb.org'],
+      }
   }
-}
